78b293924a406f626bc514ce04b697d81b70ba58
Yuji Suzuki <y0suzuki0123@gmail.com>
2017-05-28 13:32:00 +0900

    feat: add an example of trait

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,74 @@ fn hello_closure() {
     assert_eq!(2, plus_one(1));
 }
 
+fn hello_trait_without_trait() {
+    struct Rectangle {
+        x: f64,
+        y: f64,
+        width: f64,
+        height: f64,
+    }
+
+    impl Rectangle {
+        fn area(&self) -> f64 {
+            self.width * self.height
+        }
+    }
+
+    fn test() {
+        let rectangle = Rectangle {
+            x: 0f64,
+            y: 0f64,
+            width: 2f64,
+            height: 3f64,
+        };
+
+        assert_eq!(6f64, rectangle.area());
+    }
+
+    test();
+}
+
+fn hello_trait_with_trait() {
+    struct Rectangle {
+        x: f64,
+        y: f64,
+        width: f64,
+        height: f64,
+    }
+
+    trait HasArea {
+      fn area(&self) -> f64;
+    }
+
+    impl HasArea for Rectangle {
+        fn area(&self) -> f64 {
+            self.width * self.height
+        }
+    }
+
+    fn get_area<T: HasArea>(shape: T) -> f64 {
+      shape.area()
+    }
+
+    fn test() {
+        let rectangle = Rectangle {
+            x: 0f64,
+            y: 0f64,
+            width: 2f64,
+            height: 3f64,
+        };
+
+        assert_eq!(6f64, rectangle.area());
+        assert_eq!(6f64, get_area(rectangle));
+    }
+
+    test();
+}
+
 fn main() {
     println!("Hello, world!");
     hello_closure();
+    hello_trait_without_trait();
+    hello_trait_with_trait();
 }
